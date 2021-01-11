e98c3de7c23f7ea1d8361ec3c0dbdf31245f2272
mrbks1102 <[mrbks1102@gmail.com]>
2021-01-11 17:21:51 +0900

    Lesson5-1-2 Done

--- Lesson5.sql
+++ Lesson5.sql
@@ -25,3 +25,29 @@ FROM 試験結果
 WHERE 午前 >= 60
 AND 120 <= (午前1+午前2)
 AND 0.3*(午前+午後1+午後2+論述) <= 論述
+
+
+-- 問題5-2
+-- 1
+UPDATE 回答者
+SET 国名 = CASE SUBSTRING(TRIM(メールアドレス),LENGTH(TRIM(メールアドレス))-1,2)
+          WHEN 'jp' THEN '日本'
+          WHEN 'uk' THEN 'イギリス'
+          WHEN 'cn' THEN '中国'
+          WHEN 'fr' THEN 'フランス'
+          WHEN 'vn' THEN 'ベトナム' END
+
+--2
+SELECT TRIN(メールアドレス) AS メールアドレス,
+CASE WHEN 年齢 >= 20 AND 年齢 < 30 THEN ('20代')
+　　　WHEN 年齢 >= 30 AND 年齢 < 40 THEN ('30代')
+     WHEN 年齢 >= 40 AND 年齢 < 50 THEN ('40代')
+     WHEN 年齢 >= 50 AND 年齢 < 60 THEN ('60代') END
+     || ':' ||
+CASE 性別　WHEN　'M' THEN '男性'
+　　　WHEN  'F' THEN '女性'　END AS　属性
+FROM 回答者
+
+-- 問題5-3
+-- 1
+
